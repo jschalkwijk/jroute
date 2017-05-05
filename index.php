c5cb954e58f315d9017a8d2167aa34474bc0ca88
jorn <jornschalkwijk@gmail.com>
2017-05-05 15:32:33 +0200

    tested group in index, max 2 levels deep

--- index.php
+++ index.php
@@ -39,11 +39,29 @@
 			$app->get('/create', [new App\Controllers\UserController($container->db), 'one']);
 			$app->get('/delete', [new App\Controllers\UserController($container->db), 'one']);
 		});
+		$app->group('/users/profile',function($app,$container){
+			$app->get('/delete', [new App\Controllers\UserController($container->db), 'index']);
+
+		});
 		$app->group('/posts',function($app,$container){
 			$app->get('', [new App\Controllers\UserController($container->db), 'index']);
 			$app->get('/edit/:id', [new App\Controllers\UserController($container->db), 'one']);
 			$app->get('/create', [new App\Controllers\UserController($container->db), 'one']);
 			$app->get('/delete', [new App\Controllers\UserController($container->db), 'one']);
+			$app->group('/posts/profile',function($app,$container){
+				$app->get('/delete', [new App\Controllers\UserController($container->db), 'index']);
+
+			});
+		});
+		$app->group('/test',function($app,$container){
+			$app->get('', [new App\Controllers\UserController($container->db), 'index']);
+			$app->get('/edit/:id', [new App\Controllers\UserController($container->db), 'one']);
+			$app->get('/create', [new App\Controllers\UserController($container->db), 'one']);
+			$app->get('/delete', [new App\Controllers\UserController($container->db), 'one']);
+			$app->group('/test/profile',function($app,$container){
+				$app->get('/delete', [new App\Controllers\UserController($container->db), 'index']);
+
+			});
 		});
 	});
 
