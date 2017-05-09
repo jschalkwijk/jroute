3943689a399a11e67f23a7206332ee1995191d54
jorn <jornschalkwijk@gmail.com>
2017-05-09 19:18:16 +0200

    cleaned up routes file

--- routes/routes.php
+++ routes/routes.php
@@ -22,19 +22,11 @@
             $app->get('/edit/:id', [new App\Controllers\UserController($container->db), 'one']);
             $app->get('/create', [new App\Controllers\UserController($container->db), 'one']);
             $app->get('/delete', [new App\Controllers\UserController($container->db), 'one']);
-            $app->group('/posts/profile',function($app,$container){
-                $app->get('/delete', [new App\Controllers\UserController($container->db), 'index']);
-
-            });
         });
         $app->group('/test',function($app,$container){
             $app->get('', [new App\Controllers\UserController($container->db), 'index']);
             $app->get('/edit/:id', [new App\Controllers\UserController($container->db), 'one']);
             $app->get('/create', [new App\Controllers\UserController($container->db), 'one']);
             $app->get('/delete', [new App\Controllers\UserController($container->db), 'one']);
-            $app->group('/test/profile',function($app,$container){
-                $app->get('/delete', [new App\Controllers\UserController($container->db), 'index']);
-
-            });
         });
     });
